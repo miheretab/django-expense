53641d675cd52ce30a703c56df489de2db8121bc
Miheretab Alemu <mihrtab@gmail.com>
2025-07-01 15:44:50 +0400

    fix static path for render

--- djangoExpenses/settings.py
+++ djangoExpenses/settings.py
@@ -124,6 +124,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional: for local development
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
